d10eed951281ce7c4386213b7b1fa2f3d31d9b69
Martin Angers <martin.n.angers@gmail.com>
2020-04-15 20:36:59 -0400

    try decoding the args

--- main.go
+++ main.go
@@ -4,20 +4,12 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
-	"unicode"
-	"unicode/utf16"
-	"unicode/utf8"
-
-	"github.com/mattn/go-runewidth"
-	"golang.org/x/text/unicode/runenames"
 )
 
 func main() {
@@ -28,7 +20,6 @@ func main() {
 	)
 	flag.Usage = usage
 	flag.Parse()
-	_ = flagJSON
 
 	if *flagHelp || *flagLHelp {
 		help()
@@ -42,152 +33,90 @@ func main() {
 		p = &textPrinter{}
 	}
 
-	start, end := rune(12000), rune(13000)
-	//args := flag.Args()
+	args := flag.Args()
+	var rs []rune
+	for _, arg := range args {
+		if len(arg) == 0 {
+			continue
+		}
+
+		switch p0 := arg[0]; p0 {
+		case 'u', 'U', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			// it is either a number or a range
+			parts := []string{arg}
+			if rangeIx := strings.Index(arg, "-"); rangeIx >= 0 {
+				parts = []string{arg[:rangeIx], arg[rangeIx+1:]}
+			}
+
+		default:
+			rs = append(rs, runesSet(arg)...)
+		}
+	}
+
 	if err := p.printStart(os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	printRange(p, start, end)
+
 	if err := p.printEnd(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-type printer interface {
-	printStart(w io.Writer) error
-	printRune(runeInfo) error
-	printEnd() error
-}
-
-type textPrinter struct {
-	bw *bufio.Writer
-}
-
-func (tp *textPrinter) printStart(w io.Writer) error {
-	tp.bw = bufio.NewWriter(w)
-	return nil
-}
-
-func (tp *textPrinter) printRune(ri runeInfo) error {
-	var catgs string
-	if ri.Valid {
-		catgs = fmt.Sprintf("%v", ri.Categories)
-	} else {
-		catgs = "[!]"
-	}
-	fmt.Fprintf(tp.bw, "%-7s", catgs)
-
-	wd := runewidth.RuneWidth(ri.Rune)
-	rn := fmt.Sprintf("%#U", ri.Rune)
-	if n := len(rn) + wd - 1; n < 15 {
-		rn += strings.Repeat(" ", 15-n)
-	}
-	fmt.Fprintf(tp.bw, "%s", rn)
-
-	u8 := fmt.Sprintf("[% X]", ri.UTF8)
-	fmt.Fprintf(tp.bw, "%-12s", u8)
-
-	var u16 string
-	if len(ri.UTF16) == 2 {
-		u16 = fmt.Sprintf("[%X %X]", ri.UTF16[0], ri.UTF16[1])
-	} else {
-		u16 = fmt.Sprintf("[%X]", ri.UTF16[0])
-	}
-	fmt.Fprintf(tp.bw, "%-12s", u16)
-
-	fmt.Fprintln(tp.bw, ri.Name)
-	return nil
-}
-
-func (tp *textPrinter) printEnd() error {
-	return tp.bw.Flush()
-}
-
-type jsonPrinter struct {
-	ris []runeInfo
-	bw  *bufio.Writer
-}
-
-func (jp *jsonPrinter) printStart(w io.Writer) error {
-	jp.bw = bufio.NewWriter(w)
-	jp.ris = make([]runeInfo, 0, 1024) // TODO: receive a size hint
-	return nil
-}
-
-func (jp *jsonPrinter) printRune(ri runeInfo) error {
-	ri.UTF8JSON = make([]uint16, len(ri.UTF8))
-	for i, b := range ri.UTF8 {
-		ri.UTF8JSON[i] = uint16(b)
-	}
-	jp.ris = append(jp.ris, ri)
-	return nil
-}
-
-func (jp *jsonPrinter) printEnd() error {
-	enc := json.NewEncoder(jp.bw)
-	enc.SetIndent("", "  ")
-	if err := enc.Encode(jp.ris); err != nil {
-		return err
-	}
-	return jp.bw.Flush()
-}
-
-type runeInfo struct {
-	Rune       rune     `json:"rune"`
-	Name       string   `json:"name"`
-	Valid      bool     `json:"valid"`
-	Categories []string `json:"categories"`
-	UTF8       []byte   `json:"-"`
-	UTF16      []uint16 `json:"utf16"`
-	UTF8JSON   []uint16 `json:"utf8"`
-}
-
-func info(r rune) runeInfo {
-	var buf [utf8.UTFMax]byte
-
-	ri := runeInfo{Rune: r}
-	ri.Valid = utf8.ValidRune(r)
-	if !ri.Valid {
-		return ri
+// decode a command-line argument into a list of runes to print,
+// and return true as second value if this is a range in the form
+// <start>-<end> (inclusive).
+func decode(arg string) (runes []rune, isRange bool, err error) {
+	if len(arg) == 0 {
+		return nil, false, nil
 	}
 
-	ri.Name = runenames.Name(r)
-
-	n := utf8.EncodeRune(buf[:], r)
-	ri.UTF8 = buf[:n]
-	r1, r2 := utf16.EncodeRune(r)
-	if r1 == utf8.RuneError && r2 == utf8.RuneError {
-		ri.UTF16 = []uint16{uint16(r)}
-	} else {
-		ri.UTF16 = []uint16{uint16(r1), uint16(r2)}
-	}
-
-	var cats []string
-	for nm, rt := range unicode.Categories {
-		if unicode.Is(rt, r) {
-			cats = append(cats, nm)
+	p0 := arg[0]
+	base := 10
+	start := 0
+	switch p0 {
+	case 'u', 'U':
+		if len(arg) == 1 || arg[1] != '+' {
+			return runesSet(arg), false, nil
 		}
-	}
-	sort.Strings(cats)
-	ri.Categories = cats
-	return ri
-}
-
-func printRune(p printer, r rune) {
-	ri := info(r)
-	p.printRune(ri)
-}
-
-func printRange(p printer, start, end rune) {
-	for i := start; i <= end; i++ {
-		// for ranges, ignore invalid utf8 runes
-		if !utf8.ValidRune(i) {
-			continue
+		base = 16
+		start = 2 // skip u+
+	case '0':
+		if len(arg) > 1 && arg[1] == 'x' || arg[1] == 'X' {
+			base = 16
+			start = 2 // skip 0x
+			break
 		}
-		printRune(p, i)
+	case '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		// ok, decimal number
+	default:
+		return runesSet(arg), false, nil
 	}
+
+	num, err := strconv.ParseUint(arg[start:], base, 32)
+	if err != nil {
+		return nil, false, err
+	}
+	runes = append(runes, rune(num))
+	return runes, false, nil
+}
+
+// returns a slice of runes where each distinct rune in arg is returned.
+func runesSet(arg string) []rune {
+	m := make(map[rune]bool)
+	for _, r := range arg {
+		m[r] = true
+	}
+	rs := make([]rune, 0, len(m))
+	for k := range m {
+		rs = append(rs, k)
+	}
+	sort.Slice(rs, func(l, r int) bool {
+		lr, rr := rs[l], rs[r]
+		return lr < rr
+	})
+	return rs
 }
 
 func usage() {
